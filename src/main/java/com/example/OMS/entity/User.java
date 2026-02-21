3bda7ffcca625bf6016cc91f943ec6ff08d8475b
DhruvRana-anayasmi <dhruv.rana@anayasmi.in>
2026-02-21 16:32:30 +0530

    change the username constraint

--- src/main/java/com/example/OMS/entity/User.java
+++ src/main/java/com/example/OMS/entity/User.java
@@ -1,20 +1,31 @@
 package com.example.OMS.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    @Column(name="user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
     private Long userId;
+
+    @Column(unique = true, nullable = false)
     private String username;
     private String password;
     private String status;
